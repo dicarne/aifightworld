c28df8ebe13044a9eab5278117182520cb3302fc
dicarne <hzdingjiahe@outlook.com>
2018-09-15 15:01:17 +0800

    增加hero的射线检测

---
+++ Assets/Script/data/LookInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct LookInfo
+{
+
+	public string Name;
+	public string Type;
+	public Vector2 position;
+	public float angle;
+	
+}
